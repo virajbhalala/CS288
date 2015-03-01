8c500d47266a4b4cde4707df3e149ef3dcfe7873
virajbhalala <virajbhalala@gmail.com>
2015-02-28 23:54:21 -0500

    .

--- project1/insert-sort.sh
+++ project1/insert-sort.sh
@@ -1,7 +1,18 @@
 #Write a bash script that sort wihout using any built in commands
-p=(1 2 4 6 )
-lst=()
-size=$#
-lst+=$@
-echo $p
-echo $lst
+
+#One way to save all argments in the array
+lst=("$@")
+    
+for((i=1; i<$#; i++ )); do
+    j=$i
+    num=${lst[i]}
+    while [ $j -gt 0 ]; do
+	if [${lst[$j-1]} -gt $num ];then
+	    lst[$j]=lst[$j-1]
+	    let j--
+	fi
+	let j--
+    lst[j]=num
+    done
+
+done
