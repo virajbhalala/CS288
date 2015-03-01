e78fbe9b12a5940c97cb1bc7ccd81f5152c38b10
virajbhalala <virajbhalala@gmail.com>
2015-02-28 22:39:43 -0500

    .

--- project1/filer.sh
+++ project1/filer.sh
@@ -1,35 +1,37 @@
-#!/bin/sh
-
-average(){
-    sizes=( `du -hb --max-depth=1 ~ | cut -f 1` )
-    num=$((${#sizes[@]}-1))
-    sum=(${sizes[$num]})
-    array=(`du -hb --max-depth=1 ~ | cut -f 2 `)
-    avg=$((sum/num))
-    
-
-    echo ${sizes[@]}
-    echo ${array[@]}
-    echo $sum
-    echo $num
-    echo $avg
+#!/bin/bash
+function filter {
+	local dir=$1
+	local average=$2
+	for item in $(ls $dir); do
+		local size=$(du -b $dir/$item | awk '{print $1}')
+		if [ $size -gt $avg ]; then
+			echo "$item $average $size"
+		fi
+	done
 }
 
-filter()
-{ 
-    for ((i=0; i < $num; i++))
-    do
-	if (( sizes[i] >= avg ))
-	then
-	    echo ${array[i]}
-	fi
-    done
+function average {
+	local dir=$1
+	local totalSize=0
+	local numFiles=0
+#addition of all
+	for item in $(ls $dir); do
+		local size=$(du -b $dir/$item | awk '{print $1}')
+		let "totalSize+=size"
+		let "numFiles++"
+	done;
+	echo $(expr $totalSize / $numFiles)
 }
 
-main()
-{
-    average
-    filter
+function main {
+	filter $1 $(average $1)
 }
 
-main
+
+#check if length is 0
+if [ -z $1 ]; then
+	main .
+else
+#else use argument
+	main $1
+fi
